e96d5b125d8c910ad2d2ab4560bb1b2927a01e71
Yassal Sundman <yassalsundman@gmail.com>
2013-04-26 13:32:51 +0200

    #12 database and shopping list mode

--- feedme5.js
+++ feedme5.js
@@ -1,7 +1,26 @@
 List = new Meteor.Collection('list');
 
+
+
 if (Meteor.isClient) {
+    Session.set("mode", "admin");
+
+    Template.shoppingList.adminMode = function() {
+        return Session.get("mode") === "admin";
+    };
     Template.shoppingList.events({
+        'click #admin': function(e, t) {
+            Session.set("mode", "admin"); 
+        },
+        'click #shoppingList': function(e, t) {
+            Session.set("mode", "shoppingList"); 
+        }
+    });
+
+    Template.adminView.list = function() {
+        return List.find();
+    };
+    Template.adminView.events({
         'keypress input': function(e, t) {
             if (e.keyCode === 13) {
                 var input = t.find('input');
@@ -10,19 +29,31 @@ if (Meteor.isClient) {
             }
         }
     });
-    Template.item.events({
+    
+    Template.adminItem.events({
         'click .del': function(e, t) {
             List.remove({_id:t.data._id});
         },
-        'click .name': function(e, t) {
-            List.update({_id:t.data._id}, {$set: {checked:t.data.checked ? false : true}});
+        'click input[type=checkbox]': function(e, t) {
+            List.update({_id:t.data._id}, {$set: {included:t.data.included ? false : true}});
         }
     });
-    Template.shoppingList.list = function() {
-        return List.find();
-    };
-}
 
+    
+    Template.shoppingView.list = function() {
+        return List.find({included:true});
+    };
+    
+    Template.shoppingItem.events({
+        'click .name': function(e, t) {
+            List.update({_id:t.data._id}, {$set: {checked:t.data.checked ? false : true}});
+        },
+       'click .del': function(e, t) {
+            List.update({_id:t.data._id}, {$set: {included:false}});
+        }
+    });
+
+}
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
