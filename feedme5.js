56d848066bb4bd2b9bc0fdbff44e6046ee8cc187
Yassal Sundman <yassalsundman@gmail.com>
2013-04-26 15:38:53 +0200

    lint clean up

--- feedme5.js
+++ feedme5.js
@@ -1,86 +1,89 @@
-List = new Meteor.Collection('list');
+/*global Meteor, Session, Template, $ */
+(function () {
+    "use strict";
+    
+    var List = new Meteor.Collection('list');
 
-
-
-if (Meteor.isClient) {
-    Session.set("mode", "admin");
-
-    Template.shoppingList.adminMode = function() {
-        return Session.get("mode") === "admin";
-    };
-    Template.shoppingList.events({
-        'click #admin': function(e, t) {
-            Session.set("mode", "admin");
-            e.preventDefault();
-        },
-        'click #shoppingList': function(e, t) {
-            Session.set("mode", "shoppingList");
-            e.preventDefault();
-        }
-    });
-
-    Template.dbView.list = function() {
-        return List.find();
-    };
-    Template.dbView.events({
-        'keypress input': function(e, t) {
-            if (e.keyCode === 13) {
-                var input = t.find('input');
-                List.insert({name:input.value});
-                input.value = '';
+    if (Meteor.isClient) {
+        Session.set("mode", "admin");
+    
+        Template.shoppingList.adminMode = function () {
+            return Session.get("mode") === "admin";
+        };
+        Template.shoppingList.events({
+            'click #admin': function (e, t) {
+                Session.set("mode", "admin");
+                e.preventDefault();
+            },
+            'click #shoppingList': function (e, t) {
+                Session.set("mode", "shoppingList");
+                e.preventDefault();
             }
-        }
-    });
-    Template.dbItem.events({
-        'click .del': function(e, t) {
-            List.remove({_id:t.data._id});
-        },
-        'click input[type=checkbox]': function(e, t) {
-            List.update({_id:t.data._id}, {$set: {included:t.data.included ? false : true}});
-        }
-    });
-
-    Session.set("adminMode", "db");
-    Template.adminView.adminDbMode = function() {
-        return Session.get("adminMode") === "db";
-    };
-    Template.adminView.events({
-        'click #adminDb': function(e, t) {
-            Session.set("adminMode", "db"); 
-            e.preventDefault();
-        },
-        'click #adminStores': function(e, t) {
-            Session.set("adminMode", "stores"); 
-            e.preventDefault();
-        }
-    });
-    Template.storeView.rendered = function() {
-        $('ul[data-sortable="true"]').sortable({stop: function(event, ui) {
-            $(ui.item).parent().find('li').each(function(index, item) {
-                List.update({_id:$(item).attr('data-id')}, {$set: {coop:index}});
-            });
-        }});
-    };
-    Template.storeView.list = function() {
-        return List.find({}, {sort:{coop:1}});
-    };
-
-    Template.shoppingView.list = function() {
-        return List.find({included:true}, {sort:{coop:1}});
-    };
-    Template.shoppingItem.events({
-        'click .name': function(e, t) {
-            List.update({_id:t.data._id}, {$set: {checked:t.data.checked ? false : true}});
-        },
-       'click .del': function(e, t) {
-            List.update({_id:t.data._id}, {$set: {included:false}});
-        }
-    });
-
-}
-
-if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
-}
+        });
+    
+        Template.dbView.list = function () {
+            return List.find();
+        };
+        Template.dbView.events({
+            'keypress input': function (e, t) {
+                if (e.keyCode === 13) {
+                    var input = t.find('input');
+                    List.insert({name: input.value});
+                    input.value = '';
+                }
+            }
+        });
+        Template.dbItem.events({
+            'click .del': function (e, t) {
+                List.remove({_id: t.data._id});
+            },
+            'click input[type=checkbox]': function (e, t) {
+                List.update({_id: t.data._id}, {$set: {included: t.data.included ? false : true}});
+            }
+        });
+    
+        Session.set("adminMode", "db");
+        Template.adminView.adminDbMode = function () {
+            return Session.get("adminMode") === "db";
+        };
+        Template.adminView.events({
+            'click #adminDb': function (e, t) {
+                Session.set("adminMode", "db");
+                e.preventDefault();
+            },
+            'click #adminStores': function (e, t) {
+                Session.set("adminMode", "stores");
+                e.preventDefault();
+            }
+        });
+        Template.storeView.rendered = function () {
+            $('ul[data-sortable="true"]').sortable({stop: function (event, ui) {
+                $(ui.item).parent().find('li').each(function (index, item) {
+                    List.update({_id: $(item).attr('data-id')}, {$set: {coop: index}});
+                });
+            }});
+        };
+        Template.storeView.list = function () {
+            return List.find({}, {sort: {coop: 1}});
+        };
+    
+        Template.shoppingView.list = function () {
+            return List.find({included: true}, {sort: {coop: 1}});
+        };
+        Template.shoppingItem.events({
+            'click .name': function (e, t) {
+                List.update({_id: t.data._id}, {$set: {checked: t.data.checked ? false : true}});
+            },
+            'click .del': function (e, t) {
+                List.update({_id: t.data._id}, {$set: {included: false}});
+            }
+        });
+    
+    }
+    
+    if (Meteor.isServer) {
+        Meteor.startup(function () {
+        // code to run on server at startup
+        });
+    }
+}());
