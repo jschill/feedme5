8e64486163788000d8fd4e71f018fc3316ebe9ef
Yassal Sundman <yassalsundman@gmail.com>
2013-04-26 17:16:27 +0200

    #5 half way done, introduced stores

--- feedme5.js
+++ feedme5.js
@@ -1,8 +1,12 @@
 /*global Meteor, Session, Template, $ */
+
+List = new Meteor.Collection('list');
+Stores = new Meteor.Collection('stores');
+
 (function () {
     "use strict";
     
-    var List = new Meteor.Collection('list');
+
 
     if (Meteor.isClient) {
         Session.set("mode", "admin");
@@ -56,20 +60,17 @@
                 e.preventDefault();
             }
         });
-        Template.storeView.rendered = function () {
-            $('ul[data-sortable="true"]').sortable({stop: function (event, ui) {
-                $(ui.item).parent().find('li').each(function (index, item) {
-                    List.update({_id: $(item).attr('data-id')}, {$set: {coop: index}});
-                });
-            }});
-        };
-        Template.storeView.list = function () {
-            return List.find({}, {sort: {coop: 1}});
-        };
     
         Template.shoppingView.list = function () {
-            return List.find({included: true}, {sort: {coop: 1}});
+            var store = Session.get("shopByStore"), sortInfo = {};
+            if( store ) {
+                sortInfo[store] = 1;
+            }
+            return List.find({included: true}, {sort: sortInfo});
         };
+        
+        Deps.autorun(Template.shoppingView.list);
+        
         Template.shoppingItem.events({
             'click .name': function (e, t) {
                 List.update({_id: t.data._id}, {$set: {checked: t.data.checked ? false : true}});
@@ -78,7 +79,60 @@
                 List.update({_id: t.data._id}, {$set: {included: false}});
             }
         });
-    
+        
+        Template.storeView.stores = Template.shoppingView.stores = function () {
+            return Stores.find();
+        };
+        
+        Template.shopByStore.events({
+            'click .name': function (e, t) {
+                Session.set("shopByStore", t.data._id);
+            }
+        });
+
+        Template.storeView.events({
+            'keypress input': function (e, t) {
+                if (e.keyCode === 13) {
+                    var input = t.find('input');
+                    Stores.insert({name: input.value});
+                    input.value = '';
+                }
+            }
+        });
+        
+        Template.storeList.events({
+            'click .name': function (e, t) {
+                Session.set("store", t.data._id);
+            },
+            'click .del': function (e, t) {
+                Stores.remove({_id: t.data._id});
+                //clean up to remove from the List database
+            }
+        });
+
+        
+        Template.storeView.rendered = function () {
+            $('ul[data-sortable="true"]').sortable({stop: function (event, ui) {
+                var setInfo = {};
+                $(ui.item).parent().find('li').each(function (index, item) {
+                    setInfo[Session.get("store")] = index;
+                    List.update({_id: $(item).attr('data-id')}, {$set: setInfo});
+                });
+            }});
+        };
+
+        Template.storeView.storeSet = function () {
+            return !!Session.get("store");
+        }
+        
+        Template.storeView.list = function () {
+            var store = Session.get("store"), sortInfo = {};
+            if( store ) {
+                sortInfo[store] = 1;
+                return List.find({}, {sort: sortInfo});
+            }
+        };
+
     }
     
     if (Meteor.isServer) {
